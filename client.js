76611e1811feb2f8ca8e13999e257e4d0fb59530
Holly Tuhake <hollyjeanhansen@gmail.com>
2017-10-06 17:02:38 -0500

    Completed Requirement

--- client.js
+++ client.js
@@ -3,4 +3,31 @@ $(document).ready(readyNow);
 
 function readyNow() {
   console.log('ready');
+  clickHandlers();
+}
+
+var totalCost = 0;
+
+function clickHandlers(){
+
+$("#submit").on('click', storeEmployee);
+
+}
+
+function storeEmployee(){
+  var employeeInfo = new Employee ($('#firstName').val(),$('#lastName').val(),$('#idNumber').val(),$('#jobTitle').val(),$('#annualSalary').val());
+  $("table").append('<tr><td>' + employeeInfo.first + '</td><td>' + employeeInfo.last + '</td><td>'+ employeeInfo.id + '</td><td>'+ employeeInfo.title + '</td><td>' + employeeInfo.salary + '</td></tr>');
+  totalCost = totalCost + parseInt(employeeInfo.salary);
+  $("#totalCost").text(totalCost);
+  $("input").val("");
+  console.log(employeeInfo);
+}
+
+function Employee (first, last, id, title, salary) {
+  console.log('clicked submit');
+  this.first = first;
+  this.last = last;
+  this.id = id;
+  this.title = title;
+  this.salary = salary;
 }
