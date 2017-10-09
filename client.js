053f666e2b362be58f286dcf92d50476d658bbbd
Holly Tuhake <hollyjeanhansen@gmail.com>
2017-10-09 09:18:18 -0500

    cleaned up unecessary comments

--- client.js
+++ client.js
@@ -8,28 +8,14 @@ function readyNow() {
 }
 
 var totalCost = 0;
-var i = 0;
 
 function storeEmployee(){
-  //i = i + 1;
   var employeeInfo = new Employee ($('#firstName').val(),$('#lastName').val(),$('#idNumber').val(),$('#jobTitle').val(),$('#annualSalary').val());
-  //createTable();
   $("table").append("<tr><td>" + employeeInfo.first + '</td><td>' + employeeInfo.last + '</td><td>'+ employeeInfo.id + '</td><td>'+ employeeInfo.title + '</td><td>' + employeeInfo.salary + "</td><td><button class='remove'>Remove</button></td></tr>");
   totalCost = totalCost + parseInt(employeeInfo.salary);
   $("#totalCost").text(totalCost);
-  //employeeObjectsArray.push(employeeInfo);
-  //createTable();
-//  console.log(employeeObjectsArray);
   $("input").val("");
   console.log(employeeInfo);
-  //console.log(i);
-
-//var employeeObjectsArray = [];
-
-//function createTable(){
-//var table
-//for (var i=0, i < employeeObjectsArray.length, i +=1){
-  //};
 }
 
 function removeRow() {
@@ -42,12 +28,6 @@ function removeRow() {
   $(this).closest ('tr').remove ();
   console.log('removed');
   }
-
-//function updateMonthlyCost(){
-  //var row = $(this).closest('tr');
-  //console.log(row);
-
-
 
 function Employee (first, last, id, title, salary) {
   console.log('clicked submit');
