eea1e74c47d340af99b53f493d08153e027a4cdf
Holly Tuhake <hollyjeanhansen@gmail.com>
2017-10-08 20:05:50 -0500

    as far as I could get

--- client.js
+++ client.js
@@ -3,25 +3,50 @@ $(document).ready(readyNow);
 
 function readyNow() {
   console.log('ready');
-  clickHandlers();
+  $("#submit").on('click', storeEmployee);
+  $('table').on('click','.remove', removeRow);
 }
 
 var totalCost = 0;
-
-function clickHandlers(){
-
-$("#submit").on('click', storeEmployee);
-
-}
+var i = 0;
 
 function storeEmployee(){
+  //i = i + 1;
   var employeeInfo = new Employee ($('#firstName').val(),$('#lastName').val(),$('#idNumber').val(),$('#jobTitle').val(),$('#annualSalary').val());
-  $("table").append('<tr><td>' + employeeInfo.first + '</td><td>' + employeeInfo.last + '</td><td>'+ employeeInfo.id + '</td><td>'+ employeeInfo.title + '</td><td>' + employeeInfo.salary + '</td></tr>');
+  //createTable();
+  $("table").append("<tr><td>" + employeeInfo.first + '</td><td>' + employeeInfo.last + '</td><td>'+ employeeInfo.id + '</td><td>'+ employeeInfo.title + '</td><td>' + employeeInfo.salary + "</td><td><button class='remove'>Remove</button></td></tr>");
   totalCost = totalCost + parseInt(employeeInfo.salary);
   $("#totalCost").text(totalCost);
+  //employeeObjectsArray.push(employeeInfo);
+  //createTable();
+//  console.log(employeeObjectsArray);
   $("input").val("");
   console.log(employeeInfo);
+  //console.log(i);
+
+//var employeeObjectsArray = [];
+
+//function createTable(){
+//var table
+//for (var i=0, i < employeeObjectsArray.length, i +=1){
+  //};
 }
+
+function removeRow() {
+  //updateMonthlyCost();
+  $(this).closest ('tr').remove ();
+  console.log('removed');
+  }
+
+function updateMonthlyCost(){
+  var row = $(this).closest('tr');
+  //console.log(row);
+  //$(this).find('td:last-child').text()
+  console.log(row.find('td:first-child').text());
+  totalCost = totalCost - 1;
+  $('#totalCost').text(totalCost);
+}
+
 
 function Employee (first, last, id, title, salary) {
   console.log('clicked submit');
@@ -30,4 +55,5 @@ function Employee (first, last, id, title, salary) {
   this.id = id;
   this.title = title;
   this.salary = salary;
+
 }
